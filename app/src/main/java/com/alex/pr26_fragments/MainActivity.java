a3facdf5214a5adddbd7d7a6d6e322234efed9c5
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2024-03-15 19:28:46 +0100

    Primer commit

---
+++ app/src/main/java/com/alex/pr26_fragments/MainActivity.java
@@ -0,0 +1,133 @@
+package com.alex.pr26_fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.alex.pr26_fragments.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    ActivityMainBinding binding;
+
+    //Golpecitos
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private boolean toastShown = false;
+    private long lastTime = 0;
+    private float lastX, lastY, lastZ;
+    private static final int SHAKE_THRESHOLD = 300;
+    private static final int TIME_THRESHOLD = 150;
+    private static final int SHAKE_TIME_THRESHOLD = 1000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.sensors:
+                    replaceFragment(new SensorFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+        //golpecitos
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    //Golpecitos
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        long currentTime = System.currentTimeMillis();
+        long timeDifference = currentTime - lastTime;
+
+        if (timeDifference > TIME_THRESHOLD) {
+            lastTime = currentTime;
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float deltaX = x - lastX;
+            float deltaY = y - lastY;
+            float deltaZ = z - lastZ;
+
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+
+            double speed = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ) / timeDifference * 10000;
+
+            if (speed > SHAKE_THRESHOLD) {
+                long shakeTime = System.currentTimeMillis();
+                if (shakeTime - lastTime < SHAKE_TIME_THRESHOLD && !toastShown) {
+                    showToast("¡Dos golpecitos detectados!");
+                    toastShown = true;
+
+                    //Se podra volver a mostrar luego de 10 sec
+                    new Handler().postDelayed(() -> toastShown = false, 2000);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
